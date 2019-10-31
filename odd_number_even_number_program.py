d889df0a3e3372eec7c6f9978da06e4da951ee75
saravananprakash1997 <54415213+saravananprakash1997@users.noreply.github.com>
2019-10-31 22:17:52 +0530

    Add files via upload
    this is a sample tutorial for finding whether odd or even number

---
+++ odd_number_even_number_program.py
@@ -0,0 +1,6 @@
+number=int(input("Enter the number which you want to check whether it is an odd number or even number: "))
+print("")
+if (number%2==0):
+	print("The given number is an even number")
+else:
+	print("The given number is not an even number")
