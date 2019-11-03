ec9ce6e8a97dd465d40a5afa759b5025640a2f65
saravananprakash1997 <54415213+saravananprakash1997@users.noreply.github.com>
2019-11-03 17:25:04 +0530

    Add files via upload

---
+++ factorial_program.py
@@ -0,0 +1,6 @@
+number=int(input("Enter the number which you want to find the factorial :"))
+r=1
+for i in range (1, number+1,1):
+	r=r*i
+print("the factorial value of the given number is "+str(r))
+
