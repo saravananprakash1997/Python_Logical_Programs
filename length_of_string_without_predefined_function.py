dc8a22f646c1ad6577af074074565a654171752b
saravananprakash1997 <54415213+saravananprakash1997@users.noreply.github.com>
2019-11-25 22:35:29 +0530

    Add files via upload

---
+++ length_of_string_without_predefined_function.py
@@ -0,0 +1,8 @@
+#Python program to count the length of elements in the given string without the use of predefined function.
+string1=input("enter the string1:")
+count=0
+for x in string1:
+		count=count+1
+print("the length of the given string is "+str(count))
+
+
