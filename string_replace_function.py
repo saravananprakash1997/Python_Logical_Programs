f65abec68ab95be964902fe734f3c78f20f12cdb
saravananprakash1997 <54415213+saravananprakash1997@users.noreply.github.com>
2019-11-25 21:20:02 +0530

    Add files via upload

---
+++ string_replace_function.py
@@ -0,0 +1,17 @@
+#python program to replace an element of the string with otheer element.Here we replace elements v and s with $
+input_string=input("Enter the Input String:")
+print("the string before replacement is "+ input_string)
+b=input_string.replace("vs","$")
+print("the string after replacement is "+b)
+
+
+
+
+
+
+
+
+
+
+
+ 
